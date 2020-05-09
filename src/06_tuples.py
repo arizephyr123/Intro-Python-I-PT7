8d945710f10ebaf84ef6ea15256221af3e6081e9
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-05-08 19:49:59 -0700

    tuples completed

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -20,7 +20,7 @@ import math
 
 def dist(a, b):
     """Compute the distance between two x,y points."""
-    x0, y0 = a  # Destructuring assignment
+    x0, y0 = a  # Destructuring assignment. same as --> a = (x0, y0)
     x1, y1 = b
 
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
@@ -37,9 +37,14 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # YOUR CODE HERE
 
+def print_tuple(tup):
+    for el in tup:
+        print(el)
+
+
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
