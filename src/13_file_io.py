ff98d0ef8ad5f5ad2f7f6759a06f7aed3454f36f
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-05-12 19:05:27 -0700

    file_io completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+# Note: If you’re not using the with keyword, then you should call f.close() to close the file and immediately free up any system resources used by it. If you don’t explicitly close a file, Python’s garbage collector will eventually destroy the object and close the open file for you, but the file may stay open for a while.
+with open('foo.txt') as foo_file:
+    read_foo = foo_file.read()
+    print(read_foo)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +21,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar_file = open('bar.txt', 'w')
+bar_file.write(
+    "I fart in your general direction\nYour mother was a hamster and your father smelt of elderberries.\nTis' a flesh wound.")
+bar_file.close()
+
+# After a file object is closed, either by a with statement or by calling f.close(), attempts to use the file object will automatically fail. => ValueError: I/O operation on closed file.
+bar_file = open('bar.txt', 'r')
+print(bar_file.read())
